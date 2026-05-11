bf087729430500c4df80b3f9488b33995f908b48
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-05-11 13:26:02 -0700

    Update FileHandler.java

--- FileHandler.java
+++ FileHandler.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.List;
 
-public class FileService {
+public class FileHandler {
     public static void saveToFile(List<CarbonFootprint> items, String fileName) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
             for (CarbonFootprint item : items) {
