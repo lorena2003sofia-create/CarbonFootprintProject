862f4884212c267a87774e50ca83a65eaf7a19d5
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-05-10 18:01:35 -0700

    Delete CarbonFootprint.java

--- CarbonFootprint.java
+++
@@ -1,5 +0,0 @@
-public interface CarbonFootprint {
-    // Alta cohesión: Solo se encarga de definir el comportamiento de huella
-    double getCarbonFootprint();
-    String getIdentification();
-}
