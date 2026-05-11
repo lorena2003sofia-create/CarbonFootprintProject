99f996ff761c9f5c1fa3bc6f47b5a63275c4727c
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-05-10 18:23:16 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+ import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<CarbonFootprint> list = new ArrayList<>();
+
+        list.add(new Building(3000.0));
+        list.add(new Car(12.0));
+        list.add(new Bicycle("Trek"));
+
+        for (CarbonFootprint item : list) {
+            System.out.println(item.toString() + " | CO2: " + item.getCarbonFootprint() + " kg");
+        }
+
+        FileService.saveToFile(list, "carbon_data.txt");
+    }
+}
